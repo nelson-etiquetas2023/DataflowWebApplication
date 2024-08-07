bfcb926fb65917d59cd1d00fd929b8f72bebb263
nelson pino <User@WINDOWS-P4VOGQ5>
2024-08-07 19:56:52 -0400

    commit inicial

---
+++ Dataflow.Shared/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dataflow.Shared.Models
+{
+    public class Producto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+    }
+}
---
+++ Dataflow.Shared/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dataflow.Shared.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; } = null!;    
+        public string Email { get; set; }= null!;
+        public string Password { get; set; } = null!;
+        public string TypeUser { get; set; } = null!;
+        public string Departament { get; set; } = null!;
+        public bool Active { get; set; }
+
+    }
+}
